ecb4b0b011f900454df69167589f3883c6285e8f
Dean Pooler <Deanpooler@outlook.com>
2020-11-26 06:58:07 +0100

    Add command handler and imports

--- combinebot.js
+++ combinebot.js
@@ -1,20 +1,24 @@
+const {Rcon} = require('rcon-client');
+const CronJob = require('cron').CronJob;
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+
+const { prefix, token } = require('./config/config.json');
+
+const {activityUpdater} = require('./util/activityUpdater');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
-
 }
 
 client.once('ready', () => {
-  console.log('Ready');
+  console.log('Bot is ready');
+  activityUpdater(client);
 });
 
 client.on('message', message => {
